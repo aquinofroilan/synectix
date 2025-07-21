95443b3717111ed57a177323a70f030118e5a698
Froilan J Aquino <61849282+aquinofroilan@users.noreply.github.com>
2025-07-21 23:43:55 +0800

    first commit

---
+++ src/main/java/com/froilan/synectix/SynectixApplication.java
@@ -0,0 +1,13 @@
+package com.froilan.synectix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynectixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynectixApplication.class, args);
+	}
+
+}
